ad5d5f0cbbb34b7697932c6428b87e0a2ff58623
jayakrishnapalla05 <jayakrishnapalla05@gmail.com>
2023-08-29 13:40:12 +0530

    submitting code-Tue Aug 29 2023 13:40:10 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,44 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+   const [user,setUser]=useState("");
+   const [password,setPassword]=useState("");
+   const [error,setError]=useState(false);
+
+  function check(){
+    if(!user && !password)
+    {
+      setError(true);
+    }
+    else if(!user || !password)
+    {
+      setError(true);
+    }
+    else{
+      setError(false);
+      setUser("");
+      setPassword("");
+    }
+  }
+
+
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+
+        <label>Username:</label>
+        <input type="text"  value={user} onChange={(e)=>setUser(e.target.value)}/>
+        <label>Password:</label>
+        <input type="password"  value={password} onChange={(e)=>setPassword(e.target.value)}/>
+        {
+          error?<p id="error-msg">Both username and password are required.</p>:"";
+        }
+        <br/>
+        <button type="button" onClick={check}>Login</button>
     </div>
   )
 }
